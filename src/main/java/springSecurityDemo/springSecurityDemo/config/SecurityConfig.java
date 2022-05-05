cd7c2ab734fe784f3d4de26c3f5615b5f422a83a
gsmanivel@gmail.com <gsmanivel@gmail.com>
2022-05-05 07:39:45 -0400

    Section1

--- src/main/java/springSecurityDemo/springSecurityDemo/config/SecurityConfig.java
+++
@@ -1,6 +0,0 @@
-package springSecurityDemo.springSecurityDemo.config;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-}
